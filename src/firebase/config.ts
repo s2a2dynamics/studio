2f25eaed697c9c2a652d085eb20066dba888a5c3
Anibal Suarez Hernandez <dir@s2a2dynamics.com>
2025-11-14 18:21:51 +0000

    Mira la configuración que tengo en el proyecto:
    const firebaseConfig =

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:874721866774:web:def286afa955e3c165f137",
   "apiKey": "AIzaSyDxxsCwMQknHunYSC9znBezSdRrP2VZpzk",
   "authDomain": "studio-4050755951-cec5c.firebaseapp.com",
-  "storageBucket": "studio-4050755951-cec5c.appspot.com",
+  "storageBucket": "studio-4050755951-cec5c.firebasestorage.app",
   "messagingSenderId": "874721866774",
   "measurementId": "G-5G3WBE4X6T"
 };
