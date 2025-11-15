009d142204f1cdf544b5fb664ad3ebda3883e001
S2A2 Dynamics <dir@s2a2dynamics.com>
2025-11-15 23:12:42 +0100

    Remove API key from firebase config
    Removed the API key from the firebase configuration.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4050755951-cec5c",
   "appId": "1:874721866774:web:def286afa955e3c165f137",
-  "apiKey": "AIzaSyDxxsCwMQknHunYSC9znBezSdRrP2VZpzk",
+  "apiKey": "",
   "authDomain": "studio-4050755951-cec5c.firebaseapp.com",
   "storageBucket": "studio-4050755951-cec5c.firebasestorage.app",
   "messagingSenderId": "874721866774",
